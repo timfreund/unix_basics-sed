7f0c7a6088992fea37d0e5134cc1162298dff8c3
Tim Freund <tim@freunds.net>
2017-06-09 14:52:02 -0400

    create lab materials

--- lab_prep.sh
+++ lab_prep.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "lab_prep.sh needs an implementation"
+if [ ! -d ./lab ]
+then
+    echo "Creating ./lab"
+    mkdir ./lab
+fi
+
+cp .sudoers ./lab/sudoers
+
+if [ ! -f ./lab/tom_sawyer.txt ]
+then
+    echo "Downloading tom_sawyer.txt"
+    curl -s -o ./lab/tom_sawyer.txt https://www.gutenberg.org/files/74/74-0.txt
+fi
