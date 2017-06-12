3f4cb96b7b91bc736e8c05fec724cb4e382de5ba
Tim Freund <tim@freunds.net>
2017-06-12 19:10:23 -0400

    Download college scorecard data

--- lab_prep.sh
+++ lab_prep.sh
@@ -13,3 +13,9 @@ then
     echo "Downloading tom_sawyer.txt"
     curl -s -o ./lab/tom_sawyer.txt https://www.gutenberg.org/files/74/74-0.txt
 fi
+
+if [ ! -f ./lab/MERGED2013_14_PP.csv.gz ]
+then
+    echo "Downloading MERGED2013_14_PP.csv.gz"
+    curl -s -o ./lab/MERGED2013_14_PP.csv.gz https://codemuxer.com/files/unix_basics/sed/MERGED2013_14_PP.csv.gz
+fi
